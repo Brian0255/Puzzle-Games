8a696018e34bccbf1e66285aed60b7f836a278cc
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-02-08 14:54:25 -0500

    change GameEngine interface to make more sense

--- PuzzleGames/Controller/GameEngine.cpp
+++ PuzzleGames/Controller/GameEngine.cpp
@@ -1,34 +1,23 @@
 #include "GameEngine.h"
-#include"Utilities.h"
+#include"ColorUtils.h"
 #include<qpushbutton.h>
 #include<qevent.h>
 #include"ColorConstants.h"
-
-void GameEngine::startEngine() {
-}
-
-void GameEngine::resetGame() {
-}
-
-GameEngine::GameEngine() {
-}
-
-GameEngine::~GameEngine() {
-}
 
 bool GameEngine::eventFilter(QObject* watched, QEvent* event) {
     QPushButton* btn = static_cast<QPushButton*>(watched);
     if (btn && btn->isEnabled()) {
         if (event->type() == QEvent::Enter) {
-            Utilities::changeColor(btn, ColorConstants::TILE_HOVER_COLOR);
+            ColorUtils::changeColor(btn, ColorConstants::TILE_HOVER_COLOR);
         }
 
         else if (event->type() == QEvent::Leave) {
-            Utilities::changeColor(btn, ColorConstants::TILE_DEFAULT_COLOR);
+            ColorUtils::changeColor(btn, ColorConstants::TILE_DEFAULT_COLOR);
         }
     }
 
     return QObject::eventFilter(watched, event);
 }
+
 
 
--- PuzzleGames/Controller/GameEngine.h
+++ PuzzleGames/Controller/GameEngine.h
@@ -2,16 +2,18 @@
 #include<qobject.h>
 class GameEngine : public QObject {
 	Q_OBJECT
+
 signals:
 	void sendTopLeftLabelUpdate(QString newText);
 	void sendTopRightLabelUpdate(QString newText);
 	void sendStatusLabelUpdate(QString newText);
 
 public:
-	virtual void startEngine();
-	virtual void resetGame();
-	GameEngine();
-	virtual ~GameEngine();
-	virtual bool eventFilter(QObject* watched, QEvent* event);
+	virtual void startEngine() = 0;
+	virtual void resetGame() = 0;
+
+	virtual void setupTiles() = 0;
+
+	bool eventFilter(QObject* watched, QEvent* event);
 };
 
