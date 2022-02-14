a2ac4f15e5e232534759392f60584cdc4c7868f0
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-02-13 23:50:07 -0500

    add puzzles

--- PuzzleGames/Model/Coordination/CoordinationConstants.cpp
+++ PuzzleGames/Model/Coordination/CoordinationConstants.cpp
@@ -1,5 +1,4 @@
 #include"CoordinationConstants.h"
-
 std::vector<GameLayout<7>> createLayouts() {
 	std::vector<GameLayout<7>> layouts;
 	GameLayout<7> layout1;
@@ -21,7 +20,88 @@ std::vector<GameLayout<7>> createLayouts() {
 	"-X-----",
 	"-------"
 	};
-	layouts = { layout1 };
+	layout2.layout = {
+	"B-----B",
+	"----G--",
+	"-XG--X-",
+	"---XG--",
+	"-XG--X-",
+	"-------",
+	"B-----B"
+	};
+	layout3.layout = {
+	"---B---",
+	"B-----B",
+	"-BGGG--",
+	"B-GGGX-",
+	"--GGG--",
+	"---B-B-",
+	"-B----B"
+	};
+	layout4.layout = {
+	"-BBBBB-",
+	"-------",
+	"-------",
+	"-G-X-G-",
+	"--G-G--",
+	"---G---",
+	"-------"
+	};
+	layout5.layout = {
+	"G------",
+	"G------",
+	"G--BB--",
+	"--BBBB-",
+	"G--X---",
+	"G------",
+	"G------"
+	};
+	layout6.layout = {
+	"--GX--G",
+	"XB-XX--",
+	"---X--B",
+	"XXXXXXX",
+	"---XXX-",
+	"-B-XX--",
+	"GX-X-BG"
+	};
+	layout7.layout = {
+	"-------",
+	"GGXXX--",
+	"---BB--",
+	"-------",
+	"--BB---",
+	"--XXX--",
+	"-----GG"
+	};
+	layout8.layout = {
+	"B---X-G",
+	"B-X---G",
+	"B---X-G",
+	"B-----G",
+	"B-X---G",
+	"B----XG",
+	"B-X---G"
+	};
+	layout9.layout = {
+	"-------",
+	"-X-X-X-",
+	"--G-G--",
+	"-X-X-X-",
+	"--G-G--",
+	"-X-X-X-",
+	"---BBBB"
+	};
+	layout10.layout = {
+	"---B---",
+	"-------",
+	"-X-G-X-",
+	"B-GGG-B",
+	"-X-G-X-",
+	"-------",
+	"B--B---"
+	};
+	layouts = { layout1, layout2, layout3, layout4, layout5, layout6, layout7, layout8, layout9, layout10 };
 	return layouts;
 }
 
