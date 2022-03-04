18dadd55690ab5f6a6c518746f8382e4001622e5
OnlySpaghettiCode <brian.neveu2@gmail.com>
2022-03-03 22:21:26 -0500

    change certain methods in BattleshipTile to private that should be private

--- PuzzleGames/Model/Battleship/BattleshipTile.h
+++ PuzzleGames/Model/Battleship/BattleshipTile.h
@@ -12,13 +12,15 @@ private:
 	QPushButton* button;
 	int shipsIntersecting;
 	bool secret;
-public:
-	void reveal();
-	BattleshipTile(QPushButton* button);
-	BattleshipTile();
-	void calculateShipsIntersecting(std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles, std::map<QPushButton*, std::array<int, 2>>& buttonCoords);
+
 	int calculateShipsNorth(std::map<QPushButton*, std::array<int, 2>>& buttonCoords, std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles);
 	int calculateShipsSouth(std::map<QPushButton*, std::array<int, 2>>& buttonCoords, std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles);
 	int calculateShipsEast(std::map<QPushButton*, std::array<int, 2>>& buttonCoords, std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles);
 	int calculateShipsWest(std::map<QPushButton*, std::array<int, 2>>& buttonCoords, std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles);
+public:
+	void reveal();
+	void reset();
+	BattleshipTile(QPushButton* button);
+	BattleshipTile();
+	void calculateShipsIntersecting(std::array<std::array<BattleshipTile, 10>, 10>& battleshipTiles, std::map<QPushButton*, std::array<int, 2>>& buttonCoords);
 };
